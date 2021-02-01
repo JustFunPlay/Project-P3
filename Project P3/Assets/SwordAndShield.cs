07baa168df0c1cc5f690f27b09fa1071a19af6d1
JustFunPlay <78020014+JustFunPlay@users.noreply.github.com>
2021-02-01 17:02:11 +0100

    sword & shield beweegt, moet nog wel "overmove" fixen

--- Project P3/Assets/SwordAndShield.cs
+++ Project P3/Assets/SwordAndShield.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 public class SwordAndShield : MonoBehaviour
 {
     public RaycastHit hit;
+    public GameObject sword;
     public int swordDamage;
     public float swingTime;
     public float swingSpeed;
+    public Vector3 swingMove;
+    public Vector3 swingRotate;
+    public Vector3 swingMoveNormal;
+    public Vector3 swingRotateNormal;
 
     public GameObject player;
+    public GameObject shield;
     public int shieldSlow;
+    public float blockTime;
+    public Vector3 blockMove;
+    public Vector3 blockRotate;
+    public bool toBlock;
 
     // Update is called once per frame
     void Update()
@@ -19,32 +29,66 @@ public class SwordAndShield : MonoBehaviour
         {
             player.GetComponent<PlayerMovement>().moveSpeed -= shieldSlow;
             player.GetComponent<PlayerMovement>().sprintSpeed = 1;
-            //schild gaat naar voren
+            toBlock = true;
+            blockTime = 0.3f;
         }
         if (Input.GetButtonUp("Fire2"))
         {
             player.GetComponent<PlayerMovement>().moveSpeed += shieldSlow;
             player.GetComponent<PlayerMovement>().sprintSpeed = 1.5f;
-            //schild gaat terug naar normaal
+            toBlock = false;
+            blockTime = 0.3f;
         }
         if (Input.GetButton("Fire2"))
         {
             //insert shield code
         }
-        else if (Input.GetButtonDown("Fire1"))
+        else if (Input.GetButton("Fire1"))
         {
             if (swingTime <= 0)
             {
-                if (Physics.Raycast(transform.position, transform.forward, out hit, 2.5f))
+                if (blockTime <= 0)
                 {
-                    hit.collider.gameObject.GetComponent<MobHitPoints>().DoDamage(swordDamage);
+                    if (Physics.Raycast(transform.position, transform.forward, out hit, 2.5f))
+                    {
+                        hit.collider.gameObject.GetComponent<MobHitPoints>().DoDamage(swordDamage);
+                    }
+                    swingTime = swingSpeed;
                 }
-                swingTime = swingSpeed;
             }
         }
         if (swingTime > 0)
         {
+            if (swingTime > 0.8f * swingSpeed)
+            {
+                sword.transform.Translate(4 * swingMove * Time.deltaTime);
+                sword.transform.Rotate(4 * swingRotate * Time.deltaTime);
+            }
+            else
+            {
+                sword.transform.Translate(- swingMove * Time.deltaTime);
+                sword.transform.Rotate(- swingRotate * Time.deltaTime);
+            }
             swingTime -= Time.deltaTime;
+        }
+        else
+        {
+            sword.transform.localPosition = swingMoveNormal;
+            //sword.transform.localRotation = swingRotateNormal;
+        }
+        if (blockTime > 0)
+        {
+            if (toBlock == true)
+            {
+                shield.transform.Translate(blockMove * Time.deltaTime);
+                shield.transform.Rotate(blockRotate * Time.deltaTime);
+            }
+            if (toBlock == false)
+            {
+                shield.transform.Translate(-blockMove * Time.deltaTime);
+                shield.transform.Rotate(-blockRotate * Time.deltaTime);
+            }
+            blockTime -= Time.deltaTime;
         }
     }
 }

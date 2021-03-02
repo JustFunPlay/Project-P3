76fc7d109fe43480c8ff46de4b8350ec5a4a6d63
Rubenstienstra <78020104+Rubenstienstra@users.noreply.github.com>
2021-03-02 11:51:55 +0100

    pressure plate
    Ik ben bezig geweest met de pressure plate in me scene.

---
+++ Project P3/Assets/rubens Scene/Pressureplate.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pressureplate : MonoBehaviour
+{
+    public GameObject platform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision col)
+    {
+        if(col.collider.gameObject.name == "Player")
+        {
+            platform.SetActive(true);
+        }
+        else if(col.collider.gameObject.tag == "weight")
+        {
+            platform.SetActive(true);
+        }
+    }
+    private void OnCollisionExit(Collision col)
+    {
+        if(col.collider.gameObject.tag == "weight")
+        {
+            platform.SetActive(false);
+        }
+        else if(col.collider.gameObject.name == "Player")
+        {
+            if (col.collider.gameObject.tag == "weight" == true)
+            {
+                platform.SetActive(false);
+            }
+        }
+    }
+}

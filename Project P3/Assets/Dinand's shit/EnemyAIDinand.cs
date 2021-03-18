43ca5c78cb1db7e79e3498ae0452a9d191701e5c
JustFunPlay <78020014+JustFunPlay@users.noreply.github.com>
2021-03-18 16:10:18 +0100

    enemy ai v1

---
+++ Project P3/Assets/Dinand's shit/EnemyAIDinand.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAIDinand : MobHitPoints
+{
+    public NavMeshAgent agent;
+    public Transform player;
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    public Vector3 walkpoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!playerInSightRange && !playerInAttackRange) Idle();
+        else if (playerInSightRange && !playerInAttackRange) Hunt();
+        else if (playerInAttackRange && playerInSightRange) AttackPlayer();
+    }
+
+    void Idle()
+    {
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+            agent.SetDestination(walkpoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkpoint;
+
+        if (distanceToWalkPoint.magnitude < 1)
+            walkPointSet = false;
+    }
+
+    void SearchWalkPoint()
+    {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+        walkpoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomX);
+        if (Physics.Raycast(walkpoint, -transform.up, 2, whatIsGround))
+        {
+            walkPointSet = true;
+        }
+    }
+
+    void Hunt()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    void AttackPlayer()
+    {
+        agent.SetDestination(transform.position);
+        transform.LookAt(player);
+
+        if (!alreadyAttacked)
+        {
+            Attack();
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+
+    void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
+    public virtual void Attack()
+    {
+        //attack
+    }
+}

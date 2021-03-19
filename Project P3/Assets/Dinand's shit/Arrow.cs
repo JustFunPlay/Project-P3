4de8e9e688c79858c05615dc2677864a4c2d95a8
JustFunPlay <78020014+JustFunPlay@users.noreply.github.com>
2021-03-19 16:24:41 +0100

    shooting enemy v1

---
+++ Project P3/Assets/Dinand's shit/Arrow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    public Vector3 shoot;
+    public int damage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //gameObject.GetComponent<Rigidbody>().AddForce(shoot);
+    }
+
+    private void Update()
+    {
+        transform.Translate(shoot * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        collision.collider.gameObject.GetComponent<PlayerHitPoints>().DoDamage(damage);
+        Vector3 p = transform.position;
+        Rigidbody rb = collision.collider.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.AddExplosionForce(100, p, 4, 50);
+            print("knockback");
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ Project P3/Assets/Dinand's shit/ShootingEnemy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingEnemy : EnemyAIDinand
+{
+    public GameObject arrow;
+    public GameObject shootPosition;
+    public override void Attack()
+    {
+        Instantiate(arrow, shootPosition.transform.position, transform.rotation);
+    }
+}

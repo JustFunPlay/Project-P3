b8ed6e30165e9654488e65c5a165a74f0943ba9c
Rubenstienstra <78020104+Rubenstienstra@users.noreply.github.com>
2021-03-25 17:46:52 +0100

    Spike Traps!
    Ik heb een nieuwe preset gemaakt waardoor je met de trap nog niet dood gaat, maar dat ga ik waarschijnlijk morgen nog doen.

---
+++ Project P3/Assets/rubens Scene/PlayerDeath.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter(Collision col)
+    {
+        if(col.collider.gameObject.tag == "DeathTrap")
+        {
+            print("hello");
+        }
+    }
+}

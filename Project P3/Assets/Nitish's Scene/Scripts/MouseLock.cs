cc67140503312ae2af77897d08b1c1f62ae6b8be
Redbruh <69513631+Redbruh@users.noreply.github.com>
2021-03-26 15:59:33 +0100

    Nitish's level
    level gebouwd
    mouse lock gemaakt

---
+++ Project P3/Assets/Nitish's Scene/Scripts/MouseLock.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLock : MonoBehaviour
+{
+	// Start is called before the first frame update
+	
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Backspace))
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
+
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			Cursor.lockState = CursorLockMode.None;
+			Cursor.visible = true;
+		}
+	}
+}

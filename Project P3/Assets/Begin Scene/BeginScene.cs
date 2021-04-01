73f0ded976bafa9ec416dd542e93ec44fd439c58
Rubenstienstra <78020104+Rubenstienstra@users.noreply.github.com>
2021-04-01 13:22:36 +0200

    UI voor BasicScene part 2
    Dit is het extra gedeelte voor de Scene als je wil kan je er nog dingen aan toevoegen.

--- Project P3/Assets/Begin Scene/BeginScene.cs
+++ Project P3/Assets/Begin Scene/BeginScene.cs
@@ -6,27 +6,54 @@ using UnityEngine.UI;
 public class BeginScene : MonoBehaviour
 {
     public GameObject[] buttons;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public void DisableFirstMenu()
     {
-        buttons[0].SetActive(false); 
+        buttons[0].SetActive(false);
+        buttons[4].SetActive(false);
+        buttons[7].SetActive(false);
     }
-    public void EnableSecondMenu()
+    public void DisablePlayMenu()
+    {
+        buttons[1].SetActive(false);
+        buttons[2].SetActive(false);
+        buttons[3].SetActive(false);
+        buttons[6].SetActive(false);
+    }
+    public void DisableCreditsMenu()
+    {
+        buttons[5].SetActive(false);
+        buttons[6].SetActive(false);
+    }
+    public void DisableSettingsenu()
+    {
+        buttons[6].SetActive(false);
+    }
+    public void EnableFirstMenu()
+    {
+        buttons[0].SetActive(true);
+        buttons[4].SetActive(true);
+        buttons[7].SetActive(true);
+        buttons[6].SetActive(false);
+    }
+    public void EnablePlayMenu()
     {
         buttons[1].SetActive(true);
         buttons[2].SetActive(true);
         buttons[3].SetActive(true);
+        buttons[6].SetActive(true);
     }
+    public void EnableCreditsMenu()
+    {
+        buttons[5].SetActive(true);
+        buttons[6].SetActive(true);
+    }
+    public void EnableSettingsMenu()
+    {
+        buttons[6].SetActive(true);
+    }
+
+    //Dit is het gedeelte waar als op een knop drukt dat je een een andere Scene gaat.
     public void StartLevel1()
     {
         Application.LoadLevel("Ruben's Scene");
